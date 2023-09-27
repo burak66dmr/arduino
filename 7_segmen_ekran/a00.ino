6c332c97b4d529a7b084ca8460ac87857af425b5
Burak DEMİR <burak66.dmr@gmail.com>
2023-09-27 22:16:49 +0300

    updata

--- 7_segmen_ekran/a00.ino
+++ 7_segmen_ekran/a00.ino
@@ -4,14 +4,32 @@ int i = 0;
 
 void setup()
 {
-    pinMode(
+  while (i < 7)
+  {
+    pinMode(pinler[i], 1);
+    i++;
+
+  }
 
 }
 
-
 void loop()
 {
+  i = 0;
+  while (i < 7)
+  {
+    digitalWrite(pinler[i], 1);
+    i++;
+  }
 
+  delay(400);
+
+  while (i > -1)
+  {
+    digitalWrite(pinler[i], 0);
+    i--;
+  }
+  delay(400);
 
 
 }
